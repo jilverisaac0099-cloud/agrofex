ea1ca50921fcaf475deadc3077d2cf905569c4ad
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-08-13 23:15:17 -0600

    feat:create CategoryRequest

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use illuminate\validation\Rule;
 
 class CategoryRequest extends FormRequest
 {
@@ -12,7 +13,7 @@ class CategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +24,29 @@ class CategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:400'],
+            'status' => ['string', Rule:: in(['active', 'inactive'])],
+        ];
+    }
+
+
+public function messages(): array
+    {
+        return [
+            'name.string' => 'El nombre solo pemite caracteres',
+            'name.required' => 'El campo nombre es obligatorio.',
+            'name.min' => 'El minimo de caracteres es de 3',
+            'name.max' => 'El maximo de caracteres es de 50',
+
+            'description.string' => 'La descripción solo pemite caracteres',
+            'description.required' => 'El campo descripción es obligatorio.',
+            'description.min' => 'El minimo de caracteres es de 3',
+            'description.max' => 'El maximo de caracteres es de 400',
+
+            'status.required'=> 'El campo estado es obligatorio.',
+            'status.in' => 'El campo estado debe ser uno de los siguientes: activo, inactivo.',
+            'status.string' => 'El campo estado debe ser una cadena de texto.',
         ];
     }
 }
