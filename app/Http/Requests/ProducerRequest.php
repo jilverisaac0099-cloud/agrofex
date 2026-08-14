8a12708eb06a31f7ba551145221453f74a462ced
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-08-13 22:48:15 -0600

    feat:create ProducerRequest

--- app/Http/Requests/ProducerRequest.php
+++ app/Http/Requests/ProducerRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ProducerRequest extends FormRequest
 {
@@ -12,7 +13,7 @@ class ProducerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +24,54 @@ class ProducerRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'min:3', 'max:255'],
+            'gender' => ['required','min:3','max:20',Rule:: in(['male', 'female', 'other'])],
+            'telephone' => ['required', 'string','min:7', 'max:20'],
+            'address' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string' , 'min:3', 'max:400'],
+            'schedule' => ['required', 'string' , 'min:3','max:45'],
+        
+        ];
+    }
+public function messages(): array
+    {
+        return [
+            'name.string' => 'El nombre solo pemite caracteres',
+            'name.required' => 'El campo nombre es obligatorio.',
+            'name.min' => 'El minimo de caracteres es de 3',
+            'name.max' => 'El maximo de caracteres es de 50',
+
+            'last_name.string' => 'El apellido solo pemite caracteres',
+            'last_name.required' => 'El campo apellido es obligatorio.',
+            'last_name.min' => 'El minimo de caracteres es de 3',
+            'last_name.max' => 'El maximo de caracteres es de 50',
+
+            'gender.required'=> 'El campo género es obligatorio.',
+            'gender.in' => 'El campo género debe ser uno de los siguientes: mujer, hombre, otro.',
+            'gender.string' => 'El campo género debe ser una cadena de texto.',
+            'gender.min' => 'El minimo de caracteres es de 3',
+            'gender.max' => 'El maximo de caracteres es de 20',
+
+            'telephone.integer' => 'El campo teléfono debe ser un número entero.',
+            'telephone.required' => 'El campo teléfono es obligatorio.',
+            'telephone.min' => 'El minimo de caracteres es de 7',
+            'telephone.max' => 'El maximo de caracteres es de 15',
+
+            'address.string' => 'La dirección solo pemite caracteres',
+            'address.required' => 'El campo dirección es obligatorio.',
+            'address.min' => 'El minimo de caracteres es de 3',
+            'address.max' => 'El maximo de caracteres es de 255',
+
+            'description.string' => 'La descripción solo pemite caracteres',
+            'description.required' => 'El campo descripción es obligatorio.',
+            'description.min' => 'El minimo de caracteres es de 3',
+            'description.max' => 'El maximo de caracteres es de 400',
+
+            'schedule.string' => 'El horario solo pemite caracteres',
+            'schedule.required' => 'El campo horario es obligatorio.',
+            'schedule.min' => 'El minimo de caracteres es de 3',
+            'schedule.max' => 'El maximo de caracteres es de 45',
         ];
     }
 }
