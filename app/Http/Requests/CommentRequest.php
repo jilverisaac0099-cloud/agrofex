4acf34fcbf4cdc91346038002d0b4ce682d87e13
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-08-14 21:51:35 -0600

    feat:modify CommentRequest

--- app/Http/Requests/CommentRequest.php
+++ app/Http/Requests/CommentRequest.php
@@ -12,7 +12,7 @@ class CommentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,31 @@ class CommentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'product_id' => ['required', 'integer'],
+            'prducer_id' => ['required', 'integer'],
+            'customer_id' => ['required', 'integer'],
+            'text' => ['required', 'string', 'min:3', 'max:400'],
+            'qualification' => ['required', 'integer', 'min:1', 'max:5'],
         ];
     }
+
+public function messages(): array
+    {
+        return [
+            'product_id.required' => 'El campo producto es obligatorio.',
+            'prducer_id.required' => 'El campo productor es obligatorio.',
+            'customer_id.required' => 'El campo cliente es obligatorio.',
+
+            'text.string' => 'El comentario solo pemite caracteres',
+            'text.required' => 'El campo comentario es obligatorio.',
+            'text.min' => 'El minimo de caracteres es de 3',
+            'text.max' => 'El maximo de caracteres es de 400',
+
+            'qualification.required' => 'El campo calificación es obligatorio.',
+            'qualification.integer' => 'El campo calificación debe ser un número entero.',
+            'qualification.min' => 'La calificación mínima es de 1.',
+            'qualification.max' => 'La calificación máxima es de 5.',
+        ];
+    }
+
 }
