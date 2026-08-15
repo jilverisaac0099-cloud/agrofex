3df5adce89709252b6ebedb361da236b5a5dda31
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-08-15 10:18:25 -0600

    feat: create show view layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,51 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
+use App\Http\Controllers\AddressShippingController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDetailController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProducerController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Panel de control (Dashboard)
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+
+Route::resource('address_shippings', AddressShippingController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('comments', CommentController::class);
+Route::resource('customers', CustomerController::class);
+Route::resource('orders', OrderController::class);
+Route::resource('order_details', OrderDetailController::class);
+Route::resource('payments', PaymentController::class);
+Route::resource('producers', ProducerController::class);
+Route::resource('products', ProductController::class);
+Route::resource('users', UserController::class);
+
+// ---------------------------------------------------------
+// RUTAS DE PERFIL DE USUARIO (Breeze)
+ //---------------------------------------------------------
+Route::middleware('auth')->group(function () {
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
