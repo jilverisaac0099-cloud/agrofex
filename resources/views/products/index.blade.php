b15aa99fadb824969a3725f6b99b9c8dd437c78e
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-08-17 21:22:56 -0600

    fix:modify products.index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -63,6 +63,8 @@
                                             </a>
                                             <a href="{{ route('products.edit', $product) }}" class="text-gray-400 hover:text-amber-500 transition-colors" title="Editar">
                                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                                            </a>
+                                            <form action="{{ route('products.destroy', $product) }}" method="POST" class="inline" id="form-delete-{{ $product->id }}">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="button" onclick="confirmarEliminacion({{ $product->id }})" class="text-gray-400 hover:text-red-500 transition-colors" title="Eliminar">
