9ec3d92c7d79cf555a1d6ff151305066e5b4443f
jilverisaac0099-cloud <jilverisaac0099@gmail.com>
2026-08-28 21:27:57 -0600

    fix:modify AddressShippingMigrations

--- database/migrations/2026_07_22_024324_create-address_shippings-.php
+++ database/migrations/2026_07_22_024324_create-address_shippings-.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->string('municipality', 45);
             $table->string('exempt_address', 45 );
 
-            $table->integer('producer_id')->unsigned();
-            $table->foreign("producer_id")->references("id")->on("producers")->onUpdate("cascade")->onDelete("cascade");
-
             $table->bigInteger('customer_id')->unsigned();
             $table->foreign("customer_id")->references("id")->on("customers")->onUpdate("cascade")->onDelete("cascade");
             $table->timestamps();
